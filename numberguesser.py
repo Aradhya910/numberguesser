132ae877fecb80c918da5718a0f64e95342f7ab9
Aradhya910 <85386383+Aradhya910@users.noreply.github.com>
2021-07-12 21:49:48 +0530

    Add files via upload

---
+++ numberguesser.py
@@ -0,0 +1,28 @@
+import random
+number = random.randint(1, 10)
+
+
+player_name = input('What is your name? ')
+number_of_guesses = 0
+print('''Hello! ''' + player_name + " Let's start the game, Press W for instructions. " )
+instructions = input('')
+
+if instructions.upper() == 'W':
+    print(''' You have to guess a random number between 1 to 10 
+ You only have 5 number of guesses! ''')
+else:
+    print("I did not understand that, sorry ")
+
+while number_of_guesses <= 4:
+    guess = int(input())
+    number_of_guesses += 1
+    if guess < number:
+        print('The guess is low. ')
+    if guess > number:
+        print('The guess is high. ')
+    if guess == number:
+        break
+if guess == number:
+    print('You guessed the number in ' + str(number_of_guesses) + ' tries! ')
+else:
+    print('Alas you lost! The number was ' + str(number) + '. ')
